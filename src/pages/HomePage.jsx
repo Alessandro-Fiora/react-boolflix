515424dc2908a6fe39d3aba7d4b61ad499a2d63f
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-12-16 16:54:39 +0100

    feat: add ResultSection and Card components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,18 @@
 import { searchContext } from "../contexts/searchContext";
 
+import ResultSection from "../components/ResultSection.jsx";
+
 export default function HomePage() {
+  const printStars = (rating) => {
+    let resultStars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        resultStars.push(true);
+      } else resultStars.push(false);
+    }
+
+    return resultStars;
+  };
   const { series, movies, searchFields, search, setSearchFields } =
     searchContext();
 
@@ -15,17 +27,6 @@ export default function HomePage() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     search();
-  };
-
-  const printStars = (rating) => {
-    let resultStars = [];
-    for (let i = 0; i < 5; i++) {
-      if (i < rating) {
-        resultStars.push(true);
-      } else resultStars.push(false);
-    }
-
-    return resultStars;
   };
 
   return (
@@ -46,71 +47,13 @@ export default function HomePage() {
         </form>
       </div>
 
-      <div className="result-section">
-        <h2>FILM</h2>
-        {movies?.length ? (
-          movies.map((movie) => {
-            return (
-              <ul key={movie.id}>
-                <li>{movie.title}</li>
-                <li>{movie.original_title}</li>
-                <li>
-                  <span
-                    className={`lang-icon lang-icon-${movie.language}`}
-                  ></span>
-                </li>
-                <li>
-                  {printStars(movie.rating).map((star, index) =>
-                    star ? (
-                      <i key={index} className="fa-solid fa-star" />
-                    ) : (
-                      <i key={index} className="fa-regular fa-star" />
-                    )
-                  )}
-                </li>
-                <li>
-                  <img src={movie.img} alt="" />
-                </li>
-              </ul>
-            );
-          })
-        ) : (
-          <h3>Nessun film</h3>
-        )}
-      </div>
+      <ResultSection datas={movies}>
+        <h2>Film</h2>
+      </ResultSection>
 
-      <div className="result-section">
-        <h2>SERIE TV</h2>
-        {series?.length ? (
-          series.map((serie) => {
-            return (
-              <ul key={serie.id}>
-                <li>{serie.title}</li>
-                <li>{serie.original_title}</li>
-                <li>
-                  <span
-                    className={`lang-icon lang-icon-${serie.language}`}
-                  ></span>
-                </li>
-                <li>
-                  {printStars(serie.rating).map((star, index) =>
-                    star ? (
-                      <i key={index} className="fa-solid fa-star" />
-                    ) : (
-                      <i key={index} className="fa-regular fa-star" />
-                    )
-                  )}
-                </li>
-                <li>
-                  <img src={serie.img} alt="" />
-                </li>
-              </ul>
-            );
-          })
-        ) : (
-          <h3>Nessuna serie</h3>
-        )}
-      </div>
+      <ResultSection datas={series}>
+        <h2>Serie TV</h2>
+      </ResultSection>
     </div>
   );
 }
